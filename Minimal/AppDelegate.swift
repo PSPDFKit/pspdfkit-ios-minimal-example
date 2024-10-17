a6c65c69890466dd7feffe8c0b01d16b8bdb7482
PSPDFKit <support@pspdfkit.com>
2024-10-17 12:43:34 +0200

    PSPDFKit for iOS Minimal example app

---
+++ Minimal/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  Copyright © 2014-2024 PSPDFKit GmbH. All rights reserved.
+//
+//  The PSPDFKit Sample applications are licensed with a modified BSD license.
+//  Please see License for details. This notice may not be removed from this file.
+//
+
+import UIKit
+import PSPDFKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    public func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        // Set your license key here. PSPDFKit is commercial software.
+        // Each PSPDFKit license is bound to a specific app bundle id.
+        // Visit https://customers.pspdfkit.com to get your demo or commercial license key.
+        SDK.setLicenseKey("YOUR_LICENSE_KEY_GOES_HERE")
+        return true
+    }
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        configuration.sceneClass = UIWindowScene.self
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
+    }
+
+}
